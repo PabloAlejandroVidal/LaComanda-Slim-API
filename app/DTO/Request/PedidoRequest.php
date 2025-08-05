7d5ac72bca53e11f42ebf617e3cd1e7612c0ddd1
Pablo Vidal <pabloalejandrovidal@gmail.com>
2025-08-05 15:39:46 -0300

    feat: crear DTO PedidoRequest con metodo fromArray y validate

--- app/DTO/Request/PedidoRequest.php
+++ app/DTO/Request/PedidoRequest.php
@@ -2,31 +2,36 @@
 namespace App\DTO;
 
 use ValidatableRequest;
-use Respect\Validation\{
-    Validator as v,
-    Exceptions\NestedValidationException
-};
+use Respect\Validation\Validator as v;
+use Respect\Validation\Exceptions\NestedValidationException;
 
 class PedidoRequest implements ValidatableRequest
 {
     public function __construct(
-        public int $id,
-        public int $cantidad,
+        public string $nombre,
+        public string $mesa,
+        public array $detalles       // cada elemento debe ser ['id' => int, 'cantidad' => int]
     ) {}
+
     public static function fromArray(array $data): self
     {
         self::validate($data);
-        
-        return new self($data['id'], $data['cantidad']);
+
+        return new self(
+            $data['nombre'],
+            strtoupper($data['mesa']), // normalizamos a mayúscula si lo necesitás
+            $data['detalles']
+        );
     }
 
-    public static function validate(array $data): void{
-        v::key('nombre', v::alpha()->length(2, 40))
-        ->key('mesa', v::alnum()->length(5, 5))
-        ->key('detalles', v::arrayType()->each(v::key('id', v::intVal())
-        ->key('cantidad', v::intVal()->positive())))
-        ->assert($data);
-    } 
+    public static function validate(array $data): void
+    {
+        v::key('nombre', v::alpha('áéíóúÁÉÍÓÚñÑ ')->length(2, 40))
+         ->key('mesa', v::alnum()->length(5, 5))
+         ->key('detalles', v::arrayType()->each(
+             v::key('id', v::intVal())
+              ->key('cantidad', v::intVal()->positive())
+         ))
+         ->assert($data);
+    }
 }
-
-?>
